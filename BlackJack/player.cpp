ad61ffe022fead9ee3a95a46cf3ec09a9907da83
tomihararyu <tomihara12@ryu@gmail.com>
2023-11-04 20:48:02 +0900

    不等号の向きを変更

--- BlackJack/player.cpp
+++ BlackJack/player.cpp
@@ -29,11 +29,11 @@ void Player::CardOpen(int& PlayerMAXnam)
 		
 		if (PlayerHand[i] == 1)
 		{
-			if (PlayerHand[i] + PlayerMAXnam < 21)
+			if (PlayerHand[i] + PlayerMAXnam > 21)
 			{
 				PlayerHand[i] += 1;
 			}
-			else if (PlayerHand[i] + PlayerMAXnam >= 21)
+			else if (PlayerHand[i] + PlayerMAXnam <= 21)
 			{
 				PlayerMAXnam += 11;
 			}
